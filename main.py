fcb8f4e241e07cafbf818f4795ba8750d5f05b77
Max Ruffo <Max.Ruffo@Student.HTW-Berlin.de>
2023-09-22 18:12:32 +0200

    basis funktionen erreicht

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 import numpy as np
 from PIL import Image
 from tensorflow import keras
+import random
 
 # Pfade zu den Datenordnern
 train_data_dir = 'Data/train/'
 valid_data_dir = 'Data/valid/'
 bird_image_dir = 'Picture_of_Bird/'  # Pfad zum Ordner mit dem Bild des Vogels
+input_image_dir = 'Input_Images/'  # Pfad zum Ordner mit den Eingabebildern
 
 # Laden Sie ein vortrainiertes Modell (z. B. MobileNetV2) oder trainieren Sie ein Modell von Grund auf
 # Hier verwenden wir MobileNetV2 als Beispiel
@@ -35,11 +37,17 @@ def process_image(image_path, predicted_class):
     else:
         print("Bild wurde nicht verschoben.")
 
-# Hauptfunktion zum Hochladen und Verarbeiten eines Bilds
+# Hauptfunktion zum Hochladen und Verarbeiten eines zufälligen Bilds aus "Input_Images"
 def main():
-    bird_image_path = input("Geben Sie den Pfad zum Bild des zu erkennenden Vogels ein: ").strip()
-    predicted_class = predict_bird(bird_image_path)
-    process_image(bird_image_path, predicted_class[0][1])
+    # Liste aller Bilder im Ordner "Input_Images"
+    input_images = os.listdir(input_image_dir)
+    
+    # Zufälliges Bild auswählen
+    random_image = random.choice(input_images)
+    input_image_path = os.path.join(input_image_dir, random_image)
+    
+    predicted_class = predict_bird(input_image_path)
+    process_image(input_image_path, predicted_class[0][1])
 
 if __name__ == "__main__":
     main()
